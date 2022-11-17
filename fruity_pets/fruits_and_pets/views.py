f021c1ec0ba505c2bfca86e48eae0fa97a49798f
OnePartHarmony <bakingharmony@gmail.com>
2022-11-17 18:20:36 -0500

    create works for both pets and fruits

--- fruity_pets/fruits_and_pets/views.py
+++ fruity_pets/fruits_and_pets/views.py
@@ -6,7 +6,7 @@ from fruits_and_pets.serializers import FruitSerializer, PetSerializer
 
 from .models import Fruit, Pet
 
-
+# Fruit views
 class FruitsView(APIView):
     """View class at fruits/ for indexing and creating fruits."""
     def get(self, request):
@@ -14,6 +14,12 @@ class FruitsView(APIView):
         serializer = FruitSerializer(fruits, many=True)
         return Response({"fruits": serializer.data})
 
+    def post(self, request):
+        serializer = FruitSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FruitDetailView(APIView):
@@ -21,12 +27,21 @@ class FruitDetailView(APIView):
     pass
 
 
+
+# Pet views
 class PetsView(APIView):    
     """View class at pets/ for indexing and creating pets."""
     def get(self, request):
         pets = Pet.objects.all()
         serializer = PetSerializer(pets, many=True)
         return Response({"pets": serializer.data})
+
+    def post(self, request):
+        serializer = PetSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PetDetailView(APIView):
     """View class at pets/:pk/ for showing, updating, and deleting pets."""
